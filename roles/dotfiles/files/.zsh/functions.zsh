03837563f9ef29f7fbba7a3d3fd98ffea6e55757
alexcardell <29524087+alexcardell@users.noreply.github.com>
2018-01-09 20:05:21 +0000

    Zsh: Move cursor logic to functions file

--- roles/dotfiles/files/.zsh/functions.zsh
+++ roles/dotfiles/files/.zsh/functions.zsh
@@ -7,3 +7,30 @@ function t() {
     tmux new-session
   fi
 }
+
+# Decide cursor shape escape sequence
+BLOCK="\E]50;CursorShape=0\C-G"
+LINE="\E]50;CursorShape=1\C-G"
+if [[ -n $TMUX ]]; then
+  BLOCK="\EPtmux;\E\E]50;CursorShape=0\x7\E\\"
+  LINE="\EPtmux;\E\E]50;CursorShape=1\x7\E\\"
+fi
+
+# Use a line cursor for insert mode, block for normal
+function zle-keymap-select zle-line-init {
+  case $KEYMAP in
+    vicmd)      print -n -- "$BLOCK";; # block cursor
+    viins|main) print -n -- "$LINE";; # line cursor
+  esac
+  zle reset-prompt
+  zle -R
+}
+
+# Always default to block on ending a command
+function zle-line-finish {
+  print -n -- "$BLOCK"
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
