552890a5ffe4a009e370ea36bf370175a7c74b8c
alexcardell <29524087+alexcardell@users.noreply.github.com>
2018-04-06 00:00:21 +0100

    Zsh: Remove old git prompt

--- roles/dotfiles/files/.zsh/functions.zsh
+++ roles/dotfiles/files/.zsh/functions.zsh
@@ -50,34 +50,3 @@ function zle-line-finish {
 zle -N zle-line-init
 zle -N zle-line-finish
 zle -N zle-keymap-select
-
-# # Prompt
-# function prompt#git_branch() {
-#   git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
-# }
-
-# function prompt#git_status() {
-#   local STATE="$(git status --porcelain 2>/dev/null)"
-#   local OUTPUT='%F{white}|%f'
-#   # ! unstaged changes are present
-#   [[ -n $(egrep '^.[MD]' <<<"$STATE") ]] && OUTPUT="$OUTPUT%F{red}!%f"
-#   # + changes are staged and ready to commit
-#   [[ -n $(egrep '^[MADRC]' <<<"$STATE") ]] && OUTPUT="$OUTPUT%F{green}+%f"
-#   # ? untracked files are present
-#   [[ -n $(egrep '^\?\?' <<<"$STATE") ]] && OUTPUT="$OUTPUT%F{red}?%f"
-#   # ✓ local commits need to be pushed to the remote
-#   [[ -n $(git log --branches --not --remotes) ]] && OUTPUT="$OUTPUT%F{blue}✓%f"
-#   echo $OUTPUT
-# }
-
-# function prompt#git() {
-#   # get branch name
-#   local GIT_BRANCH=$(prompt#git_branch)
-#   # if empty we're not in a git repo
-#   if [[ -n $GIT_BRANCH ]]; then
-#     GIT_BRANCH="%F{magenta}$GIT_BRANCH%f"
-#     local GIT_STATE=$(prompt#git_status)
-#     # combine branch and state info
-#     echo -e "%F{white}(%f$GIT_BRANCH$GIT_STATE%F{white})%f"
-#   fi
-# }
