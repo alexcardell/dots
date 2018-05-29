ce6b056da41340d89266e038a8e4af5bd02735b9
alexcardell <29524087+alexcardell@users.noreply.github.com>
2018-05-29 11:25:04 +0100

    Zsh: add initnvm alias

--- roles/dotfiles/files/.zsh/aliases.zsh
+++ roles/dotfiles/files/.zsh/aliases.zsh
@@ -6,6 +6,7 @@ alias dc='docker-compose'
 alias killtomcat='pkill -9 -f tomcat'
 alias cdp='cd $(git rev-parse --show-toplevel 2> /dev/null || echo -n ".")'
 alias py='python'
+alias initnvm=". /usr/local/opt/nvm/nvm.sh"
 
 alias tat='tmux attach\; choose-tree -s'
 
