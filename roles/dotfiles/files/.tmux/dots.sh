280e384146f825c08868423c97dbfca6fec616c4
alexcardell <29524087+alexcardell@users.noreply.github.com>
2018-10-12 17:25:59 +0100

    Unignore .tmux scripts

---
+++ roles/dotfiles/files/.tmux/dots.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+name=dots
+
+if tmux has-session -t $name 2> /dev/null; then
+  tmux attach -t $name
+  exit
+fi
+
+cd ~/dots
+
+tmux new-session -d -s $name -n edit
+
+# 1. Editing
+tmux send-keys -t edit "vim" Enter
+
+tmux new-window -n term
+tmux send-keys -t term "git status" Enter
+
+tmux attach -t $name:edit.1
