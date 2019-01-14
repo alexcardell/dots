f4c319d69217f781186a2a75d24929c7b120b5bf
alexcardell <29524087+alexcardell@users.noreply.github.com>
2019-01-14 14:03:55 +0000

    [brew] Overwrite xcode git with brew git

--- roles/dotfiles/files/.zsh/path.zsh
+++ roles/dotfiles/files/.zsh/path.zsh
@@ -4,8 +4,10 @@ unset PATH
 PATH=$HOME/bin
 PATH=$PATH:$HOME/local/bin
 PATH=$PATH:$HOME/.zsh/bin
-PATH=$PATH:/usr/local/bin
 PATH=$PATH:/usr/local/sbin
+PATH=$PATH:/usr/local/bin
+# use project specific node modules
+PATH=node_modules/.bin:$PATH
 PATH=$PATH:$OLD_PATH
 
 typeset -U PATH
