31f9f2ef5255c26be2fdab048b9c0d541c835745
alexcardell <29524087+alexcardell@users.noreply.github.com>
2019-03-07 16:09:28 +0000

    [tmux] Update almanac tmux script

--- roles/dotfiles/files/.tmux/almanac.sh
+++ roles/dotfiles/files/.tmux/almanac.sh
@@ -8,12 +8,24 @@ if tmux has-session -t $name 2> /dev/null; then
   exit
 fi
 
-cd ~/Code/$name
+cd ~/Code/js/$name
 
 tmux new-session -d -s $name -n edit
 
 # 1. Editing
-tmux split-window -h -t $name:edit
-tmux send-keys -t $name:edit.2 "git status" Enter
+tmux send-keys -t $name:edit "vim" Enter
+
+#2. Shell
+tmux new-window -n term
+tmux send-keys -t $name:term "git status" Enter
+
+#3. Build
+tmux new-window -n build
+tmux split-window -h -t $name:build
+tmux split-window -v -t $name:build.1
+
+tmux send-keys -t $name:build.1 "yarn dev:client" Enter
+tmux send-keys -t $name:build.2 "yarn sass --watch" Enter
+tmux send-keys -t $name:build.3 "yarn dev:server" Enter
 
 tmux attach -t $name:edit.1
