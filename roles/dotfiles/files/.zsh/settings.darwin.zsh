cafcc43d14ac241ac49c11f693b50b523b02f756
alexcardell <29524087+alexcardell@users.noreply.github.com>
2019-04-14 22:18:10 +0100

    [zsh] Add gcloud settings source for darwin

--- roles/dotfiles/files/.zsh/settings.darwin.zsh
+++ roles/dotfiles/files/.zsh/settings.darwin.zsh
@@ -1,3 +1,6 @@
 # AWS credentials
 export RW_AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
 export RW_AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
+
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
