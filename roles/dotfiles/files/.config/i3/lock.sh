d52cd54f881f8466a3570533ed53982ede53fee0
alexcardell <29524087+alexcardell@users.noreply.github.com>
2019-12-19 19:17:02 +0000

    Update wallpaper

--- roles/dotfiles/files/.config/i3/lock.sh
+++ roles/dotfiles/files/.config/i3/lock.sh
@@ -3,5 +3,5 @@
 # scrot -o /tmp/screen.png
 # convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 # convert /tmp/screen.png ~/Pictures/archlinux-small.png -gravity center -composite -matte /tmp/screen.png
-i3lock -t -i ~/Pictures/wallpaper.png
+i3lock -t -i $1
 # rm /tmp/screen.png
