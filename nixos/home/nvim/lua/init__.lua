0f0b7108aea77e8e6a58a5f29e9bc003e53e9a01
alexcardell <29524087+alexcardell@users.noreply.github.com>
2022-04-18 20:32:02 +0100

    fixup! nixos: Add nvim-metals

--- nixos/home/nvim/lua/init__.lua
+++ nixos/home/nvim/lua/init__.lua
@@ -1,5 +1,5 @@
 -- This module is functionally an init.lua
--- and is imported via home-manager 
+-- and is imported via home-manager
 -- see nixos/home.nix
 
 vim.g.mapleader = " " -- space
@@ -17,7 +17,7 @@ lsp.setup_lsp()
 vim.cmd([[
   augroup lsp
     au!
-    au FileType java,scala,sbt lua require("metals").initialize_or_attach(require('lsp').metals)
+    au FileType java,scala,sbt lua require("metals").initialize_or_attach(require('alex/lsp').metals)
   augroup end
 ]])
 
@@ -33,3 +33,13 @@ vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {})
 vim.api.nvim_set_keymap("n", "<leader>fc", "<cmd>Telescope commands<cr>", {})
 vim.api.nvim_set_keymap("n", "<leader>fm", "<cmd>Telescope marks<cr>", {})
 vim.api.nvim_set_keymap("n", "<leader>fp", "<cmd>Telescope registers<cr>", {})
+
+vim.api.nvim_set_keymap("n", "<localleader>d", "<cmd>lua vim.lsp.buf.definition()<cr>", {})
+vim.api.nvim_set_keymap("n", "<localleader>t", "<cmd>lua vim.lsp.buf.type_definition()<cr>", {})
+vim.api.nvim_set_keymap("n", "<localleader>h", "<cmd>lua vim.lsp.buf.hover()<cr>", {})
+vim.api.nvim_set_keymap("n", "<localleader>H", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>", {})
+vim.api.nvim_set_keymap("n", "<localleader>i", "<cmd>lua vim.lsp.buf.implementation()<cr>", {})
+vim.api.nvim_set_keymap("n", "<localleader>R", "<cmd>lua vim.lsp.buf.rename()<cr>", {})
+vim.api.nvim_set_keymap("n", "<localleader>r", "<cmd>lua vim.lsp.buf.references()<cr>", {})
+vim.api.nvim_set_keymap("n", "<localleader>c", "<cmd>lua vim.lsp.buf.code_action()<cr>", {})
+vim.api.nvim_set_keymap("n", "<localleader>l", "<cmd>lua vim.lsp.codelens.run()<cr>", {})
