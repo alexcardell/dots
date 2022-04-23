6e66678ba813df8e8f46a35ac078922a146f8e7e
alexcardell <29524087+alexcardell@users.noreply.github.com>
2022-04-23 11:43:35 +0100

    nvim: Set number/rnu

--- nixos/home/nvim/lua/init__.lua
+++ nixos/home/nvim/lua/init__.lua
@@ -4,19 +4,23 @@
 
 vim.g.mapleader = " " -- space
 vim.g.maplocalleader = "\\"
-
-local key = vim.api.nvim_set_keymap
-key("i", "jk", "<Esc>", {})
-
 vim.g.hidden = true
 vim.opt_global.shortmess:remove("F") -- required by metals
+vim.opt.number = true
+vim.opt.relativenumber = true
 
+local key = vim.api.nvim_set_keymap
+
+key("i", "jk", "<Esc>", {})
+
+-- LSP
 local completion = require('alex/completion')
 local lsp = require('alex/lsp')
 
 completion.setup()
 lsp.setup_lsp()
 
+-- metals autocmd
 vim.cmd([[
   augroup lsp
     au!
@@ -24,9 +28,35 @@ vim.cmd([[
   augroup end
 ]])
 
+key("n", "<localleader>d", "<cmd>lua vim.lsp.buf.definition()<cr>", {})
+key("n", "<localleader>t", "<cmd>lua vim.lsp.buf.type_definition()<cr>", {})
+key("n", "<localleader>h", "<cmd>lua vim.lsp.buf.hover()<cr>", {})
+key("n", "<localleader>H", "<cmd>lua vim.diagnostic.open_float()<cr>", {})
+key("n", "<localleader>i", "<cmd>lua vim.lsp.buf.implementation()<cr>", {})
+key("n", "<localleader>R", "<cmd>lua vim.lsp.buf.rename()<cr>", {})
+key("n", "<localleader>r", "<cmd>lua vim.lsp.buf.references()<cr>", {})
+key("n", "<localleader>c", "<cmd>lua vim.lsp.buf.code_action()<cr>", {})
+key("n", "<localleader>l", "<cmd>lua vim.lsp.codelens.run()<cr>", {})
+
+-- Telescope
+key("n", "<leader>fa", "<cmd>Telescope find_files<cr>", {})
+key("n", "<leader>f/", "<cmd>Telescope live_grep<cr>", {})
+key("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {})
+key("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {})
+key("n", "<leader>fc", "<cmd>Telescope commands<cr>", {})
+key("n", "<leader>fm", "<cmd>Telescope marks<cr>", {})
+key("n", "<leader>fp", "<cmd>Telescope registers<cr>", {})
+
+
+-- tmux integration
+require('Navigator').setup()
+key('n', "<C-h>", '<CMD>NavigatorLeft<CR>', {})
+key('n', "<C-l>", '<CMD>NavigatorRight<CR>', {})
+key('n', "<C-k>", '<CMD>NavigatorUp<CR>', {})
+key('n', "<C-j>", '<CMD>NavigatorDown<CR>', {})
+
 -- Tree
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
 vim.fn.sign_define( "DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
 vim.fn.sign_define( "DiagnosticSignWarn", {text = " ", texthl = "DiagnosticSignWarn"})
 vim.fn.sign_define( "DiagnosticSignInfo", {text = " ", texthl = "DiagnosticSignInfo"})
@@ -58,30 +88,3 @@ require('neo-tree').setup({
 })
 
 key("n", "<leader>t", "<cmd>Neotree toggle reveal=true<cr>", {})
-
--- Telescope
-key("n", "<leader>fa", "<cmd>Telescope find_files<cr>", {})
-key("n", "<leader>f/", "<cmd>Telescope live_grep<cr>", {})
-key("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {})
-key("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {})
-key("n", "<leader>fc", "<cmd>Telescope commands<cr>", {})
-key("n", "<leader>fm", "<cmd>Telescope marks<cr>", {})
-key("n", "<leader>fp", "<cmd>Telescope registers<cr>", {})
-
--- LSP
-key("n", "<localleader>d", "<cmd>lua vim.lsp.buf.definition()<cr>", {})
-key("n", "<localleader>t", "<cmd>lua vim.lsp.buf.type_definition()<cr>", {})
-key("n", "<localleader>h", "<cmd>lua vim.lsp.buf.hover()<cr>", {})
-key("n", "<localleader>H", "<cmd>lua vim.diagnostic.open_float()<cr>", {})
-key("n", "<localleader>i", "<cmd>lua vim.lsp.buf.implementation()<cr>", {})
-key("n", "<localleader>R", "<cmd>lua vim.lsp.buf.rename()<cr>", {})
-key("n", "<localleader>r", "<cmd>lua vim.lsp.buf.references()<cr>", {})
-key("n", "<localleader>c", "<cmd>lua vim.lsp.buf.code_action()<cr>", {})
-key("n", "<localleader>l", "<cmd>lua vim.lsp.codelens.run()<cr>", {})
-
--- tmux integration
-require('Navigator').setup()
-key('n', "<C-h>", '<CMD>NavigatorLeft<CR>', {})
-key('n', "<C-l>", '<CMD>NavigatorRight<CR>', {})
-key('n', "<C-k>", '<CMD>NavigatorUp<CR>', {})
-key('n', "<C-j>", '<CMD>NavigatorDown<CR>', {})
