6d7c88b46c8ba35631a79a4e1e0ff1a915623fe9
alexcardell <29524087+alexcardell@users.noreply.github.com>
2022-04-23 22:42:21 +0100

    nvim: Add diff and diagnostics to statusline

--- nixos/home/nvim/lua/alex/statusline.lua
+++ nixos/home/nvim/lua/alex/statusline.lua
@@ -10,7 +10,7 @@ local colors = {
 
 local bubbles_theme = {
   normal = {
-    a = { fg = colors.white, bg = colors.grey },
+    a = { fg = colors.grey, bg = colors.white },
     b = { fg = colors.white, bg = colors.grey },
     c = { fg = colors.black, bg = colors.black },
   },
@@ -18,6 +18,7 @@ local bubbles_theme = {
   insert = { a = { fg = colors.black, bg = colors.blue } },
   visual = { a = { fg = colors.black, bg = colors.cyan } },
   replace = { a = { fg = colors.black, bg = colors.red } },
+  command = { a = { fg = colors.black, bg = colors.violet } },
 
   inactive = {
     a = { fg = colors.white, bg = colors.black },
@@ -38,10 +39,10 @@ M.setup = function ()
       lualine_a = {
         { 'mode', separator = { left = '' }, right_padding = 2 },
       },
-      lualine_b = { 'filename', 'branch' },
-      lualine_c = { 'fileformat' },
-      lualine_x = {},
-      lualine_y = { 'filetype', 'progress' },
+      lualine_b = { 'filename', 'branch', 'diff' },
+      lualine_c = { },
+      lualine_x = { },
+      lualine_y = { 'diagnostics', 'filetype', 'progress' },
       lualine_z = {
         { 'location', separator = { right = '' }, left_padding = 2 },
       },
