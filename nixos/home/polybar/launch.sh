b12bfb4c905f566aac4c20cbf85330b0d740a2be
alexcardell <29524087+alexcardell@users.noreply.github.com>
2022-05-28 18:43:50 +0100

    i3: Increase polybar size

--- nixos/home/polybar/launch.sh
+++ nixos/home/polybar/launch.sh
@@ -3,7 +3,8 @@
 # killall -q polybar
 
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=~/.config/polybar/config.ini \
+  example 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
