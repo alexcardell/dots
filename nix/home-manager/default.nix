4879eeec4d425805c6106e88635a9ff8b72ddf43
alexcardell <29524087+alexcardell@users.noreply.github.com>
2022-07-11 13:47:53 +0100

    darwin: Fix tmux

--- nix/home-manager/default.nix
+++ nix/home-manager/default.nix
@@ -23,7 +23,7 @@
     coursier
     curl
     fd
-    fzf
+    # fzf
     gh
     git
     git-crypt
@@ -40,11 +40,42 @@
     sbt
     tmux
     unstable.neovim
+    nodejs-14_x
+    nodePackages.typescript-language-server
+    yarn
   ];
 
   programs.java = {
     enable = true;
     package = pkgs.jdk8;
     # (pkgs.jdk8.overrideAttrs (_: { postPatch = "rm man; ln -s ../zulu-8.jdk/Contents/Home/man man"; }));
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv = {
+      enable = true;
+      enableFlakes = true;
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+
+    sessionVariables = {
+      EDITOR = "nvim";
+      ZVM_VI_ESCAPE_BINDKEY = "jk";
+    };
+
+    envExtra = builtins.readFile ./home/zsh/.zshenv;
+    initExtra = builtins.readFile ./home/zsh/.zshrc;
+
+    plugins = [
+      {
+        name = "zsh-vi-mode";
+        src = pkgs.zsh-vi-mode;
+        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+      }
+    ];
   };
 }
