dc9bfe173a647a5df16e2cfe375469da8147b4dc
alexcardell <29524087+alexcardell@users.noreply.github.com>
2022-11-13 16:23:33 +0000

    nixos: Add torrent packages

--- nixos/systems/linux/home/default.nix
+++ nixos/systems/linux/home/default.nix
@@ -3,7 +3,11 @@
   home.packages = with pkgs; [
     bitwarden
     bitwarden-cli
+    qbittorrent
     rofi
+    vlc
+    protonvpn-cli_2
+    brightnessctl
   ];
 
   programs.firefox.enable = true;
