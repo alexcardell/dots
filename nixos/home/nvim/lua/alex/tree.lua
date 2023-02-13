d7a56780bdd5d412b1d301f40a59fe034c46a085
alexcardell <29524087+alexcardell@users.noreply.github.com>
2023-02-13 11:35:24 +0000

    nvim: Add more nvim-tree mappings

--- nixos/home/nvim/lua/alex/tree.lua
+++ nixos/home/nvim/lua/alex/tree.lua
@@ -9,7 +9,23 @@ M.setup = function()
       mappings = {
         custom_only = true,
         list = {
+          -- :help nvim-tree-default-mappings
           { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
+          { key = { "<C-v>" }, action = "vsplit" },
+          { key = { "<C-x>" }, action = "split" },
+          { key = { "<C-t>" }, action = "tabnew" },
+          { key = { "R" }, action = "refresh" },
+          { key = { "a" }, action = "create" },
+          { key = { "d" }, action = "remove" },
+          { key = { "r" }, action = "rename" },
+          { key = { "-" }, action = "dir_up" },
+          { key = { "\\[" }, action = "prev_diag_item" },
+          { key = { "\\]" }, action = "next_diag_item" },
+          { key = { "[c" }, action = "prev_git_item" },
+          { key = { "]c" }, action = "next_git_item" },
+          { key = { "K" }, action = "prev_sibling" },
+          { key = { "J" }, action = "next_sibling" },
+          { key = { "H" }, action = "toggle_dotfiles" },
           { key = "g?", action = "toggle_help" },
         }
       }
