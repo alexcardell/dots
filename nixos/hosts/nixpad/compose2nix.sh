ceb6e0dc1b948a4ee9f6c224be2c09f93dccbe67
alexcardell <29524087+alexcardell@users.noreply.github.com>
2024-08-07 17:45:18 +0100

    nix: Add jellyfin docker-compose

---
+++ nixos/hosts/nixpad/compose2nix.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+nix run github:aksiksi/compose2nix -- -inputs homelab-compose.yaml -output homelab-compose.nix
--- nixos/hosts/nixpad/homelab.nix
+++ nixos/hosts/nixpad/homelab.nix
@@ -1,8 +1,22 @@
 { config, pkgs, ... }:
 {
-  services.jellyfin.enable = true;
-  services.sonarr.enable = true;
-  services.radarr.enable = true;
-  services.lidarr.enable = true;
-  services.prowlarr.enable = true;
+
+  imports =
+    [ 
+      ./homelab-compose.nix
+    ];
+  # environment.systemPackages = with pkgs;[
+  #   # jellyfin
+  #   jellycli
+  #   jellyfin-web
+  #   # jellyfin-ffmpeg
+  # ];
+  # services.jellyfin = {
+  #   enable = true;
+  #   # user = "alex";
+  # };
+  # services.sonarr.enable = true;
+  # services.radarr.enable = true;
+  # services.lidarr.enable = true;
+  # services.prowlarr.enable = true;
 }
