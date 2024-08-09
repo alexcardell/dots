cef99d22808e0fd340975f34918091024fdadd83
alexcardell <29524087+alexcardell@users.noreply.github.com>
2024-08-09 17:11:04 +0100

    nvim: Move hardtime setup

--- nixos/home/nvim/lua/init__.lua
+++ nixos/home/nvim/lua/init__.lua
@@ -11,6 +11,7 @@ require('alex/plugins/telescope').setup()
 require('alex/plugins/gitsigns').setup()
 require('alex/plugins/navigator').setup()
 require('alex/plugins/oil').setup()
-require('alex/plugins/ogpt').setup()
-require('hardtime').setup()
+require('alex/plugins/hardtime').setup()
+
+-- keymappings
 require('alex/keys')
