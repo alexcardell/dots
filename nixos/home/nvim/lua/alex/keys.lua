1150a0014dd6920cbdf7e9270cd07e7147cc04e4
alexcardell <29524087+alexcardell@users.noreply.github.com>
2024-08-29 10:41:28 +0100

    nvim: Remove expr from gitsigns navigation bindings
    For some reason this is broken so we won't have diff navigation
    in diff view for now

--- nixos/home/nvim/lua/alex/keys.lua
+++ nixos/home/nvim/lua/alex/keys.lua
@@ -9,8 +9,8 @@ key('n', '<leader>hs', '<cmd>Gitsigns stage_hunk<cr>', {desc = 'Stage hunk under
 key('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>', {desc = 'Undo staging hunk'})
 key({'n','v'}, '<leader>hr', '<cmd>Gitsigns reset_hunk<cr>', {desc = 'Reset hunk under cursor'})
 key('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<cr>', {desc = 'Preview hunk under cursor in popup window'})
-key('n', ']c', '&diff ? "]c" : "<cmd>Gitsigns next_hunk<cr>"', {desc='Move to next hunk', expr = true })
-key('n', '[c', '&diff ? "[c" : "<cmd>Gitsigns prev_hunk<cr>"', {desc='Move to next hunk', expr = true })
+key('n', ']c', '<cmd>Gitsigns next_hunk<cr>', {desc='Move to next hunk'})
+key('n', '[c', '<cmd>Gitsigns prev_hunk<cr>', {desc='Move to next hunk'})
 key({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', {desc='Select hunk as text object'})
 
 -- Telescope
