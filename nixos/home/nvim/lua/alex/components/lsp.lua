8941369d0c0509fdd3a800ac5178dd921d983ca2
alexcardell <29524087+alexcardell@users.noreply.github.com>
2024-08-29 15:55:55 +0100

    nvim: Update ltex-ls default language

--- nixos/home/nvim/lua/alex/components/lsp.lua
+++ nixos/home/nvim/lua/alex/components/lsp.lua
@@ -5,43 +5,19 @@ local M = {}
 
 local capabilities = require('alex/components/completion').capabilities
 
-M.setup = function()
-  local default_on_attach = function(client, bufnr)
-    -- disable semantic tokens
-    client.server_capabilities.semanticTokensProvider = nil
+local default_on_attach = function(client, bufnr)
+  -- disable semantic tokens
+  client.server_capabilities.semanticTokensProvider = nil
 
-    -- inlay_hints.on_attach(client, bufnr, true)
+  -- inlay_hints.on_attach(client, bufnr, true)
 
-    -- if client.server_capabilities.documentSymbolProvider then
-    --   navic.attach(client, bufnr)
-    -- end
-  end
-  -- vim.lsp.inlay_hint.enable()
+  -- if client.server_capabilities.documentSymbolProvider then
+  --   navic.attach(client, bufnr)
+  -- end
+end
 
-  -- anything with standard empty configuration
-  local standard_servers = {
-    'ltex',
-    'lua_ls',
-    'nixd',
-    'smithy_ls',
-    'terraformls',
-    'tsserver',
-  }
 
-  lspconfig.util.default_config = vim.tbl_extend(
-    "force",
-    lspconfig.util.default_config,
-    {
-      capabilities = capabilities,
-    }
-  )
-
-  for _, server in ipairs(standard_servers) do
-    lspconfig[server].setup({
-      on_attach = default_on_attach
-    })
-  end
-
+local setup_metals = function()
   local metals = metals_.bare_config()
 
   metals.init_options = {
@@ -73,6 +49,44 @@ M.setup = function()
       metals_.initialize_or_attach(metals)
     end,
   })
+end
+
+M.setup = function()
+  lspconfig.util.default_config = vim.tbl_extend(
+    "force",
+    lspconfig.util.default_config,
+    {
+      capabilities = capabilities,
+    }
+  )
+
+  -- anything with standard empty configuration
+  local standard_servers = {
+    'lua_ls',
+    'nixd',
+    'smithy_ls',
+    'terraformls',
+    'tsserver',
+  }
+
+  for _, server in ipairs(standard_servers) do
+    lspconfig[server].setup({
+      on_attach = default_on_attach
+    })
+  end
+
+  lspconfig.ltex.setup({
+    on_attach = default_on_attach,
+    filetypes = { 'markdown', 'tex' },
+    settings = {
+      ltex = {
+        language = "en-GB",
+        motherTongue = "en-GB"
+      },
+    },
+  })
+
+  setup_metals()
 end
 
 return M
