7167c5b1551510aca16ced3e175bb03f6fbef27a
alexcardell <29524087+alexcardell@users.noreply.github.com>
2024-08-30 16:20:01 +0100

    nvim: Extend <tab> to jump in snippet

--- nixos/home/nvim/lua/alex/components/completion.lua
+++ nixos/home/nvim/lua/alex/components/completion.lua
@@ -1,21 +1,45 @@
 local M = {}
 
+
 M.setup = function()
   local cmp = require('cmp')
-  local cmp_lsp = require('cmp_nvim_lsp')
+  local luasnip = require('luasnip')
 
   cmp.setup({
     snippet = {
       expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        luasnip.lsp_expand(args.body) -- For `luasnip` users.
       end,
     },
     mapping = cmp.mapping.preset.insert({
       -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
       -- ['<C-Space>'] = cmp.mapping.complete(),
-      -- ['<C-e>'] = cmp.mapping.abort(),
-      ['<Tab>'] = cmp.mapping.confirm({ select = true, behavior = 'replace' }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<C-c>'] = cmp.mapping.abort(),
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          if luasnip.expandable() then
+            luasnip.expand()
+          else
+            -- Accept currently selected item.
+            -- Set `select` to `false` to only confirm explicitly selected items.
+            cmp.confirm({ select = true, behavior = 'replace' })
+          end
+        elseif luasnip.locally_jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, {"i"}),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i" }),
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
