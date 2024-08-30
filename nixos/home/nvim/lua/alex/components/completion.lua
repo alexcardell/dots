a845ae94e95b9b512db0003de84c077b8e738cc7
alexcardell <29524087+alexcardell@users.noreply.github.com>
2024-08-30 16:21:04 +0100

    nvim: Add more completion mappings

--- nixos/home/nvim/lua/alex/components/completion.lua
+++ nixos/home/nvim/lua/alex/components/completion.lua
@@ -11,10 +11,9 @@ M.setup = function()
       end,
     },
     mapping = cmp.mapping.preset.insert({
-      -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      -- ['<C-Space>'] = cmp.mapping.complete(),
-      -- ['<C-e>'] = cmp.mapping.abort(),
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-c>'] = cmp.mapping.abort(),
       ['<Tab>'] = cmp.mapping.confirm({ select = true, behavior = 'replace' }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
