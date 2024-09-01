2708a01c6d622ec0db0f3136425b6d9fac1e062c
alexcardell <29524087+alexcardell@users.noreply.github.com>
2024-09-01 21:38:33 +0100

    nvim: Configure outline

--- nixos/home/nvim/lua/alex/plugins/outline.lua
+++ nixos/home/nvim/lua/alex/plugins/outline.lua
@@ -2,10 +2,20 @@ local M = {}
 
 M.setup = function()
   require('outline').setup({
+    outline_window = {
+      position = 'left',
+      show_numbers = true,
+      show_relative_numbers = true,
+    },
+    outline_items = {
+      show_symbol_lineno = true,
+    },
     symbols = {
       icon_source = 'lspkind'
     },
-    keymaps = {}
+    keymaps = {
+      show_help = 'g?',
+    }
   })
 end
 
