0e99c20c3a828c1f944ab69dc158986e47bf0d04
alexcardell <29524087+alexcardell@users.noreply.github.com>
2025-01-21 10:36:12 +0000

    nix: Remove generated comments

--- nixos/hosts/nixpad/configuration.nix
+++ nixos/hosts/nixpad/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -17,7 +13,7 @@
     "1.1.1.1"
     "1.0.0.1"
   ];
-  networking.firewall.allowedTCPPorts = [ 
+  networking.firewall.allowedTCPPorts = [
     8123 # home-assistant
     9999 # qbittorrent
   ];
