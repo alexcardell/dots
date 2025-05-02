4afdb37da88b5121466ba7f31f4669ddad56cdeb
alexcardell <29524087+alexcardell@users.noreply.github.com>
2025-05-02 10:03:25 +0100

    nvim: Add light-mode colours

--- nixos/home/nvim/lua/alex/plugins/base16.lua
+++ nixos/home/nvim/lua/alex/plugins/base16.lua
@@ -19,7 +19,7 @@ local colours = {
   brown = '#a3685a'
 }
 
-local base16_colours = {
+local dark_mode = {
   base00 = colours.black,
   base01 = colours.base01,
   base02 = colours.base02,
@@ -37,6 +37,27 @@ local base16_colours = {
   base0E = colours.violet,
   base0F = colours.brown
 }
+
+local light_mode = {
+  base00 = '#ffffff',
+  base01 = '#e0e0e0',
+  base02 = '#d6d6d6',
+  base03 = '#8e908c',
+  base04 = '#969896',
+  base05 = '#4d4d4c',
+  base06 = '#282a2e',
+  base07 = '#1d1f21',
+  base08 = '#c82829',
+  base09 = '#f5871f',
+  base0A = '#eab700',
+  base0B = '#718c00',
+  base0C = '#3e999f',
+  base0D = '#4271ae',
+  base0E = '#8959a8',
+  base0F = '#a3685a'
+}
+
+local base16_colours = dark_mode
 
 M.colours = colours
 
