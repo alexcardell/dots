ce570076ec3b59586494f245005691b6469045c1
alexcardell <29524087+alexcardell@users.noreply.github.com>
2025-10-17 15:06:11 +0100

    nvim: Disable neoscroll

--- nixos/home/nvim/lua/init__.lua
+++ nixos/home/nvim/lua/init__.lua
@@ -38,7 +38,7 @@ require('alex/plugins/outline').setup()
 require('alex/plugins/marks').setup()
 require('alex/plugins/lualine').setup()
 require('alex/plugins/render-markdown').setup()
-require('alex/plugins/neoscroll').setup()
+-- require('alex/plugins/neoscroll').setup()
 
 -- keymappings
 require('alex/keys')
