e6dd314749537bc26c21fffbd1afd3cd975aa209
alexcardell <29524087+alexcardell@users.noreply.github.com>
2025-11-10 12:51:45 +0000

    nix(darwin): Add karabiner-elements service

--- nixos/hosts/darwin/configuration.nix
+++ nixos/hosts/darwin/configuration.nix
@@ -35,4 +35,21 @@
     name = "alexcard";
     home = "/Users/alexcard";
   };
+
+  services.karabiner-elements = {
+    enable = true;
+    # use older karabiner-elements version
+    # awaiting fix https://github.com/nix-darwin/nix-darwin/issues/1041
+    package = pkgs.karabiner-elements.overrideAttrs (old: {
+      version = "14.13.0";
+
+      src = pkgs.fetchurl {
+        inherit (old.src) url;
+        hash = "sha256-gmJwoht/Tfm5qMecmq1N6PSAIfWOqsvuHU8VDJY8bLw=";
+      };
+
+      dontFixup = true;
+    });
+  };
+
 }
