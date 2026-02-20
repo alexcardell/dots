52afefd7bd5dace837a0738151b931ee6c83f3c7
alexcardell <29524087+alexcardell@users.noreply.github.com>
2026-02-20 11:24:33 +0000

    nvim: Land on top file in new oil directory buffers

--- nixos/home/nvim/lua/alex/plugins/oil.lua
+++ nixos/home/nvim/lua/alex/plugins/oil.lua
@@ -1,6 +1,39 @@
 local M = {}
 
 M.setup = function()
+  local function focus_first_oil_entry(bufnr)
+    local winid = vim.fn.bufwinid(bufnr)
+    if winid == -1 then
+      return
+    end
+
+    if vim.api.nvim_buf_line_count(bufnr) < 2 then
+      return
+    end
+
+    local cursor = vim.api.nvim_win_get_cursor(winid)
+    if cursor[1] ~= 1 then
+      return
+    end
+
+    local ok, first_entry = pcall(require("oil").get_entry_on_line, bufnr, 1)
+    if ok and first_entry and (first_entry.id == 0 or first_entry.name == "..") then
+      vim.api.nvim_win_set_cursor(winid, { 2, 0 })
+    end
+  end
+
+  vim.api.nvim_create_autocmd("User", {
+    desc = "Start on first entry when oil enters a directory",
+    group = vim.api.nvim_create_augroup("oil-initial-cursor", { clear = true }),
+    pattern = "OilEnter",
+    callback = function(args)
+      local bufnr = args.data and args.data.buf or args.buf
+      vim.schedule(function()
+        focus_first_oil_entry(bufnr)
+      end)
+    end,
+  })
+
   -- Declare a global function to retrieve the current directory
   function _G.get_oil_winbar()
     local dir = require("oil").get_current_dir()
