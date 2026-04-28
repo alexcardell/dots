4378cbe867bb4192aa0f39d749eb5df9bf069836
alexcardell <29524087+alexcardell@users.noreply.github.com>
2026-04-28 15:38:15 +0100

    nix(darwin): Extract homebrew to file

--- nixos/systems/darwin/configuration.nix
+++ nixos/systems/darwin/configuration.nix
@@ -1,34 +1,10 @@
 { ... }:
 {
 
+  imports = [
+    ./homebrew.nix
+  ];
+
   ids.gids.nixbld = 350;
 
-  homebrew = {
-    enable = true;
-    taps = [
-      "smithy-lang/tap" # smithy-cli
-      "flipt-io/brew"
-      "snyk/tap"
-    ];
-    casks = [
-      "alt-tab"
-      "discord"
-      "firefox"
-      "malwarebytes"
-      "obsidian"
-      "docker-desktop"
-      "todoist-app"
-    ];
-    brews = [
-      "snyk"
-      "docker-credential-helper-ecr"
-      "flipt"
-      "smithy-cli"
-      "whalebrew"
-      # "terragrunt"
-      # "tfenv"
-    ];
-  };
-
-  # documentation.doc.enable = false;
 }
---
+++ nixos/systems/darwin/homebrew.nix
@@ -0,0 +1,31 @@
+{ ... }:
+{
+
+  homebrew = {
+    enable = true;
+    taps = [
+      "smithy-lang/tap" # smithy-cli
+      "flipt-io/brew"
+      "snyk/tap"
+    ];
+    casks = [
+      "alt-tab"
+      "discord"
+      "firefox"
+      "malwarebytes"
+      "obsidian"
+      "docker-desktop"
+      "todoist-app"
+    ];
+    brews = [
+      "snyk"
+      "docker-credential-helper-ecr"
+      "flipt"
+      "smithy-cli"
+      "whalebrew"
+      # "terragrunt"
+      # "tfenv"
+    ];
+  };
+
+}
