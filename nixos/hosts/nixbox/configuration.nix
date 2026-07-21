3807c1f60bcfc73856f5bf32a8c1db22cb522783
alexcardell <29524087+alexcardell@users.noreply.github.com>
2026-07-21 15:26:32 +0100

    nix(nixbox): Downgrade nvidia driver

--- nixos/hosts/nixbox/configuration.nix
+++ nixos/hosts/nixbox/configuration.nix
@@ -120,7 +120,7 @@
 
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
   };
 
   services.xserver = {
